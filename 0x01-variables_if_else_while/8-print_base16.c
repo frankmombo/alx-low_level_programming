92243604bf0bf29ddd36dc967a03bd866a9ececb
frankmombo <fumbu.com@gmail.com>
2022-03-12 19:16:33 +0300

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * Description: Prints alphanumericals
+ * Return: 0
+ */
+int main(void)
+{
+	char i = '0';
+	char c = 'a';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+© 2022 GitHub, Inc.
